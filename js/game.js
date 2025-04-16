fc0503ced51ad61d49165e004216dfa093d97441
JSousa <joaofdr.sousa@gmail.com>
2025-04-16 09:59:57 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const controls = document.getElementById('playerInputs');
-const powerFactor = 0.3; //queres mais rapido, aumentas, queres mais baixo, diminuis
+const powerFactor = 0.5; //queres mais rapido, aumentas, queres mais baixo, diminuis
 
 
 
